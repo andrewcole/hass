594b3516f155b672193355be224783060a93ff99
Andrew Cole <andrew.cole@illallangi.com>
2022-08-21 19:36:34 +1000

    Added influxdb to configuration.yaml

--- custom-cont-init.d/10-update-secrets.sh
+++ custom-cont-init.d/10-update-secrets.sh
@@ -8,3 +8,8 @@ touch "${secrets}"
 echo Configuring recorder
 yq d -i "${secrets}" recorder
 yq w -i "${secrets}" --prettyPrint recorder_db_url -- mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?charset=utf8mb4
+
+echo Configuring InfluxDB
+yq w -i "${secrets}" --prettyPrint influxdb_host     -- ${INFLUXDB_HOST}
+yq w -i "${secrets}" --prettyPrint influxdb_port     -- ${INFLUXDB_PORT}
+yq w -i "${secrets}" --prettyPrint influxdb_database -- ${INFLUXDB_DATABASE}
