cb2bd8bdb807a01056ddae72349b8eefc3971845
Andrew Cole <andrew.cole@illallangi.com>
2022-08-21 18:53:19 +1000

    Added update-secrets script

---
+++ custom-cont-init.d/10-update-secrets.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bash
+# Updates secret.yaml
+set -e
+
+secrets="/config/secrets.yaml"
+touch "${secrets}"
+
+echo Configuring recorder
+yq d -i "${secrets}" recorder
+yq w -i "${secrets}" --prettyPrint recorder_url -- mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?charset=utf8mb4
