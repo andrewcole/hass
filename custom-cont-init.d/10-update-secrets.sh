412df0433810ab7913b65652b5960b61c30ffbd4
Andrew Cole <andrew.cole@illallangi.com>
2022-08-21 19:54:29 +1000

    Added homeassistant configuration

--- custom-cont-init.d/10-update-secrets.sh
+++ custom-cont-init.d/10-update-secrets.sh
@@ -5,6 +5,12 @@ set -e
 secrets="/config/secrets.yaml"
 touch "${secrets}"
 
+echo Configuring Home Assistant
+yq w -i "${haConfiguration}" --prettyPrint homeassistant_elevation -- ${HOMEASSISTANT_ELEVATION}
+yq w -i "${haConfiguration}" --prettyPrint homeassistant_latitude  -- ${HOMEASSISTANT_LATITUDE}
+yq w -i "${haConfiguration}" --prettyPrint homeassistant_longitude -- ${HOMEASSISTANT_LONGITUDE}
+yq w -i "${haConfiguration}" --prettyPrint homeassistant_timezone  -- ${TZ}
+
 echo Configuring recorder
 yq d -i "${secrets}" recorder
 yq w -i "${secrets}" --prettyPrint recorder_db_url -- mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?charset=utf8mb4
